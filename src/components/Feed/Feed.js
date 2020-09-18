866f8899962e35ad724505d01725c4ab29bd54b1
Vaibhav <vaibhavdeep46@gmail.com>
2020-09-18 15:38:43 +0530

    sidebar component ui

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Feed.scss";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed_header">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
